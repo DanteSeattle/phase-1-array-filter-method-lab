af9ecdf221281b677aeb684323aa28581f3df347
DanteSeattle <gabrieldantelombardi@gmail.com>
2021-04-06 18:08:52 -0700

    commit tree 3bcafc0c941c83444fd179a0f48e7b329bd08831

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(arrayOfNames, name){
+    let isNamed = arrayOfNames.filter(myFunction => myFunction.toLowerCase() == name.toLowerCase());
+    
+
+    return !!isNamed ? isNamed : [];
+}
+
+function fuzzyMatch(arrayOfNames, char){
+    let isChar = arrayOfNames.filter(myFunction => myFunction.charAt(0) == char.charAt(0))
+    return isChar
+}
+
+function matchName(arrayOfObjNames, stringName){
+    let objName = arrayOfObjNames.filter(myFunction => myFunction.name == stringName);
+    return objName;
+}
